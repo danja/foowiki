e7b0e1ccd3579bc745bb76fb6b1ef3b4fbbbb3ee
Danny Ayers <danny.ayers@gmail.com>
2015-01-09 11:56:10 +0100

    bugfixes and refactoring

--- js/utils.js
+++ js/utils.js
@@ -34,3 +34,87 @@
                 });
        //     console.log("edit link = "+$('#editLink').href);
         }
+
+
+function escape(markup){
+	markup = markup.replace(/&/g, "&amp;");
+	markup = markup.replace(/</g, "&lt;");
+	markup = markup.replace(/>/g, "&gt;");
+	return markup;
+}
+
+        function xmlToEntry(pageURI, xml) {
+            var xmlString = (new XMLSerializer()).serializeToString(xml);
+
+            // workaround for wrong interpretation of charset
+            xmlString = xmlString.replace(/[^\u0000-\u007F]/g, '');
+            // maybe force to ISO-8859-1, also known as Latin-1 instead?
+
+            var $xml = $(xmlString);
+            var entry = "";
+            
+             var results = $xml.find("result");
+            
+            if (results.length == 0) { 
+                return false;
+            }
+
+            results.each(function () {
+                var uri, date, title, content, nick;
+
+                //			$(this).find("binding[name='uri']").each(function() {
+                //				uri = $(this).text().trim();
+                //			});
+                uri = "page.html?uri=" + pageURI;
+
+                $(this).find("binding[name='date']").each(function () {
+                    date = $(this).text().trim();
+                });
+                $(this).find("binding[name='title']").each(function () {
+                    title = $(this).text().trim();
+                });
+                $(this).find("binding[name='content']").each(function () {
+                    content = $(this).text().trim();
+                });
+                $(this).find("binding[name='nick']").each(function () {
+                    nick = $(this).text().trim();
+                });
+
+                entry += generateEntry(uri, date, title, content, nick);
+            });
+            return entry;
+        }
+
+ function xmlToRows(xml) {
+var xmlString = (new XMLSerializer()).serializeToString(xml);
+
+                // workaround for wrong interpretation of charset
+                xmlString = xmlString.replace(/[^\u0000-\u007F]/g, '');
+                // maybe force to ISO-8859-1, also known as Latin-1 instead?
+
+                var $xml = $(xmlString);
+                var rows = "";
+
+                $xml.find("result").each(function () {
+                    var uri, date, title, nick; // content,
+
+                    $(this).find("binding[name='uri']").each(function () {
+                        uri = $(this).text().trim();
+                    });
+                    $(this).find("binding[name='date']").each(function () {
+                        date = $(this).text().trim();
+                    });
+                    $(this).find("binding[name='title']").each(function () {
+                        title = $(this).text().trim();
+                    });
+                    //	$(this).find("binding[name='content']").each(function() {
+                    //		content = $(this).text().trim();
+                    //	});
+                    $(this).find("binding[name='nick']").each(function () {
+                        nick = $(this).text().trim();
+                    });
+
+                    rows += generatePageRow(uri, date, title, nick); // content, 
+                });
+     return rows;
+ }
