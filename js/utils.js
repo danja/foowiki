736115e13fe5f0d9f1a67b879420fc7b15d0fbcb
Danny Ayers <danny.ayers@gmail.com>
2015-01-10 14:08:36 +0100

    fixed anchor issue, refactoring

--- js/utils.js
+++ js/utils.js
@@ -9,9 +9,23 @@
                 },
                 success: function () {}
             });
-        }       
+        }
 
-function escapeRegExp(string) {
+        /* ultra-simple templating engine
+         * replacementMap contains { "name": value, ... }
+         * every instance of ${name} in template gets replaced with value
+         *
+         * (it could be maybe done with map() and/or some kind of reflection, but I reckon this is clearer
+         */
+        function templater(template, replacementMap) {
+            for (var name in replacementMap) {
+                var reg = new RegExp("\\$\\{" + name + "\\}", "g");
+                template = template.replace(reg, replacementMap[name]);
+            }
+            return template;
+        }
+
+        function escapeRegExp(string) {
             return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
         }
 
@@ -19,6 +33,7 @@ function escapeRegExp(string) {
             return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
         }
 
+        /* parse URL */
         var queryString = (function (a) {
             if (a == "") return {};
             var b = {};
@@ -36,16 +51,51 @@ function escapeRegExp(string) {
         function translateLocalLinks() {
             $('div.content  a').each(
                 function () {
+                    console.log("this = " + this);
                     if (this.href.indexOf(serverBaseURI) == 0) {
-                        console.log("old href = " + this.href);
-                        var localRef = this.href.substring(serverBaseURI.length);
-                        console.log("new = " + this.href.substring(serverBaseURI.length));
-                        console.log("new href = " + this.href);
-                        this.href = serverBaseURI + "page.html?uri=" + pagesBaseURI + localRef;
+                        var hashPosition = this.href.indexOf("#");
+                        if (hashPosition != -1) {
+                            var anchor = this.href.substring(hashPosition); // "#Something"
+                            
+                            anchor = anchor.toLowerCase().replace(/^\s+|\s+$/g,"");
+                            //.replace(/\w+/g, '-')
+                            console.log("anchor = " + anchor);
+
+                            $(this).click(function () {
+                                console.log("CLICKED anchor = " + anchor);
+                                //    window.scrollTo(0, $(anchor).offset());
+                                console.log("offset = " + $(anchor).offset());
+                                $('html, body').animate({
+                                    scrollTop: $(anchor).offset().top
+                                }, 250);
+                            });
+                        } else {
+                            //  console.log("old href = " + this.href);
+                            var localRef = this.href.substring(serverBaseURI.length);
+                            //   console.log("new = " + this.href.substring(serverBaseURI.length));
+                            //  console.log("new href = " + this.href);
+                            this.href = serverBaseURI + "page.html?uri=" + pagesBaseURI + localRef;
+                        }
                     }
 
                 });
             //     console.log("edit link = "+$('#editLink').href);
+        }
+
+         // little workaround for odd marked.js behaviour, at least in part due to marked.js line 793 regex
+         // if the header is a link, the id ends up as "-like-this-like-this-" 
+        function fixHeaderIDs() {
+            $("h2").each(function () {
+                var id = $(this).attr("id");
+                var length = id.length;
+                if (id[0] == "-" && id[length - 1] == "-") {
+                    //      console.log("need to fix");
+                    id = id.substring(1, length/2);
+                    $(this).attr("id", id);
+                }
+            //    console.log("ID = " + id);
+                
+            });
         }
 
         function escape(markup) {
