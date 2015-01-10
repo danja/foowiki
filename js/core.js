cdc3e281becc9a4ad9a452ea842fb666bfcba551
Danny Ayers <danny.ayers@gmail.com>
2015-01-10 11:11:21 +0100

    refactored some common code out into core.js

---
+++ js/core.js
@@ -0,0 +1,97 @@
+ function getPage(doneCallback, getPageURL) {
+
+
+     $.ajax({
+         url: getPageURL,
+         accept: {
+             xml: 'application/xml;charset=UTF-8',
+             sparql: 'sparql-results+xml;charset=UTF-8'
+         },
+         headers: { // belt and braces
+             'Accept': 'sparql-results+xml;charset=UTF-8'
+             //   'Accept-Charset': 'UTF-8' unsafe
+         }
+
+     }).done(function (xml) {
+         doneCallback(xml);
+     });
+ }
+
+ function xmlToEntry(xml, pageURI) {
+     
+     console.log("XML = "+xml);
+     
+     var xmlString = (new XMLSerializer()).serializeToString(xml);
+
+     // workaround for wrong interpretation of charset
+     xmlString = xmlString.replace(/[^\u0000-\u007F]/g, '');
+     // maybe force to ISO-8859-1, also known as Latin-1 instead?
+
+     var $xml = $(xmlString);
+     var entry = "";
+
+     var results = $xml.find("result");
+
+     if (results.length == 0) {
+         return false;
+     }
+
+     results.each(function () {
+         var uri, date, title, content, nick;
+
+         //			$(this).find("binding[name='uri']").each(function() {
+         //				uri = $(this).text().trim();
+         //			});
+         uri = "page.html?uri=" + pageURI;
+
+         $(this).find("binding[name='date']").each(function () {
+             date = $(this).text().trim();
+         });
+         $(this).find("binding[name='title']").each(function () {
+             title = $(this).text().trim();
+         });
+         $(this).find("binding[name='content']").each(function () {
+             content = $(this).text().trim();
+         });
+         $(this).find("binding[name='nick']").each(function () {
+             nick = $(this).text().trim();
+         });
+
+         entry += generateEntry(uri, date, title, content, nick);
+     });
+     return entry;
+ }
+
+ function xmlToRows(xml) {
+     var xmlString = (new XMLSerializer()).serializeToString(xml);
+
+     // workaround for wrong interpretation of charset
+     xmlString = xmlString.replace(/[^\u0000-\u007F]/g, '');
+     // maybe force to ISO-8859-1, also known as Latin-1 instead?
+
+     var $xml = $(xmlString);
+     var rows = "";
+
+     $xml.find("result").each(function () {
+         var uri, date, title, nick; // content,
+
+         $(this).find("binding[name='uri']").each(function () {
+             uri = $(this).text().trim();
+         });
+         $(this).find("binding[name='date']").each(function () {
+             date = $(this).text().trim();
+         });
+         $(this).find("binding[name='title']").each(function () {
+             title = $(this).text().trim();
+         });
+         //	$(this).find("binding[name='content']").each(function() {
+         //		content = $(this).text().trim();
+         //	});
+         $(this).find("binding[name='nick']").each(function () {
+             nick = $(this).text().trim();
+         });
+
+         rows += generatePageRow(uri, date, title, nick); // content, 
+     });
+     return rows;
+ }
--- js/utils.js
+++ js/utils.js
@@ -1,120 +1,56 @@
-        function escapeRegExp(string) {
+        function spinner() {
+            var $loading = $('#spinner').hide();
+            $.ajaxSetup({
+                beforeSend: function () {
+                    $('#spinner').show();
+                },
+                complete: function () {
+                    $('#spinner').hide();
+                },
+                success: function () {}
+            });
+        }       
+
+function escapeRegExp(string) {
             return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
         }
-        
+
         function replaceAll(string, find, replace) {
             return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
         }
-        
-        var queryString = (function(a) {
+
+        var queryString = (function (a) {
             if (a == "") return {};
-                           var b = {};
-                           for (var i = 0; i < a.length; ++i)  {
-                               var p=a[i].split('=', 2);
-                               if (p.length == 1)
-                                   b[p[0]] = "";
-                               else
-                                   b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-                           }
-                           return b;
+            var b = {};
+            for (var i = 0; i < a.length; ++i) {
+                var p = a[i].split('=', 2);
+                if (p.length == 1)
+                    b[p[0]] = "";
+                else
+                    b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+            }
+            return b;
         })(window.location.search.substr(1).split('&'));
-        
-        
+
+
         function translateLocalLinks() {
             $('div.content  a').each(
-                function(){
+                function () {
                     if (this.href.indexOf(serverBaseURI) == 0) {
-                   console.log("old href = "+this.href);
+                        console.log("old href = " + this.href);
                         var localRef = this.href.substring(serverBaseURI.length);
-                       console.log("new = "+this.href.substring(serverBaseURI.length));
-                      console.log("new href = "+this.href);
-                        this.href = serverBaseURI + "page.html?uri="+pagesBaseURI+localRef;
+                        console.log("new = " + this.href.substring(serverBaseURI.length));
+                        console.log("new href = " + this.href);
+                        this.href = serverBaseURI + "page.html?uri=" + pagesBaseURI + localRef;
                     }
-                    
+
                 });
-       //     console.log("edit link = "+$('#editLink').href);
+            //     console.log("edit link = "+$('#editLink').href);
         }
 
-
-function escape(markup){
-	markup = markup.replace(/&/g, "&amp;");
-	markup = markup.replace(/</g, "&lt;");
-	markup = markup.replace(/>/g, "&gt;");
-	return markup;
-}
-
-        function xmlToEntry(pageURI, xml) {
-            var xmlString = (new XMLSerializer()).serializeToString(xml);
-
-            // workaround for wrong interpretation of charset
-            xmlString = xmlString.replace(/[^\u0000-\u007F]/g, '');
-            // maybe force to ISO-8859-1, also known as Latin-1 instead?
-
-            var $xml = $(xmlString);
-            var entry = "";
-            
-             var results = $xml.find("result");
-            
-            if (results.length == 0) { 
-                return false;
-            }
-
-            results.each(function () {
-                var uri, date, title, content, nick;
-
-                //			$(this).find("binding[name='uri']").each(function() {
-                //				uri = $(this).text().trim();
-                //			});
-                uri = "page.html?uri=" + pageURI;
-
-                $(this).find("binding[name='date']").each(function () {
-                    date = $(this).text().trim();
-                });
-                $(this).find("binding[name='title']").each(function () {
-                    title = $(this).text().trim();
-                });
-                $(this).find("binding[name='content']").each(function () {
-                    content = $(this).text().trim();
-                });
-                $(this).find("binding[name='nick']").each(function () {
-                    nick = $(this).text().trim();
-                });
-
-                entry += generateEntry(uri, date, title, content, nick);
-            });
-            return entry;
+        function escape(markup) {
+            markup = markup.replace(/&/g, "&amp;");
+            markup = markup.replace(/</g, "&lt;");
+            markup = markup.replace(/>/g, "&gt;");
+            return markup;
         }
-
- function xmlToRows(xml) {
-var xmlString = (new XMLSerializer()).serializeToString(xml);
-
-                // workaround for wrong interpretation of charset
-                xmlString = xmlString.replace(/[^\u0000-\u007F]/g, '');
-                // maybe force to ISO-8859-1, also known as Latin-1 instead?
-
-                var $xml = $(xmlString);
-                var rows = "";
-
-                $xml.find("result").each(function () {
-                    var uri, date, title, nick; // content,
-
-                    $(this).find("binding[name='uri']").each(function () {
-                        uri = $(this).text().trim();
-                    });
-                    $(this).find("binding[name='date']").each(function () {
-                        date = $(this).text().trim();
-                    });
-                    $(this).find("binding[name='title']").each(function () {
-                        title = $(this).text().trim();
-                    });
-                    //	$(this).find("binding[name='content']").each(function() {
-                    //		content = $(this).text().trim();
-                    //	});
-                    $(this).find("binding[name='nick']").each(function () {
-                        nick = $(this).text().trim();
-                    });
-
-                    rows += generatePageRow(uri, date, title, nick); // content, 
-                });
-     return rows;
- }
