3a0b547458e62b09f53c503c9a42540ccaac54d1
Danny Ayers <danny.ayers@gmail.com>
2021-07-19 23:28:12 +0200

    deleted a couple of empty comments

--- js/foowiki/html-templates.js
+++ js/foowiki/html-templates.js
@@ -8,13 +8,6 @@
  * http://twitter.github.io/hogan.js/
  */
 
-/**
- * Comment template.
- * @param {string} foo This is a param with a description too long to fit in
- *     one line.
- * @return {number} This returns something that has a description too long to
- *     fit in one line.
- */
 var entryTableTemplate = " \n\
         <table id='pagesTable'> \n\
             <tr> \n\
