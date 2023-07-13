0402d53ffc2721bdf10c74b834d3a1232bb936c7
Danny Ayers <danny.ayers@gmail.com>
2023-07-13 17:36:35 +0200

    tweaks

--- js/foowiki/config.js
+++ js/foowiki/config.js
@@ -1,14 +1,14 @@
-   /*
-    *    System constants (URLs/URIs/IRIs)
-    */
+/*
+ *    System constants (URLs/URIs/IRIs)
+ */
 
-   /**
-    * Comment template.
-    * @param {string} foo This is a param with a description too long to fit in
-    *     one line.
-    * @return {number} This returns something that has a description too long to
-    *     fit in one line.
-    */
+/**
+ * Comment template.
+ * @param {string} foo This is a param with a description too long to fit in
+ *     one line.
+ * @return {number} This returns something that has a description too long to
+ *     fit in one line.
+ */
 
 // recursive
 // find ./ -type f -readable -writable -exec sed -i "s/FooWiki.preformatFormats/FooWiki.FooWiki.preformatFormats/g" {} \;
@@ -17,21 +17,24 @@
 //  var getPageUrl = FooWiki.sparqlQueryEndpoint + encodeURIComponent(getPageSparql) + "&output=xml";
 //  serverHostURI: "http://localhost:3030"
 
-   var FooWiki = {
-       pagesBaseURI: "http://hyperdata.it/wiki/",
-       graphURI: "http://hyperdata.it/wiki",
+var FooWiki = {
+    pagesBaseURI: "http://hyperdata.it/wiki/",
+    graphURI: "http://hyperdata.it/wiki",
 
-       serverRootPath: "/foowiki/",
-       sparqlQueryEndpoint: "http://hyperdata.it:3030/foowiki/sparql?query=", // move ? part ??
-       sparqlUpdateEndpoint: "http://fuseki.local:3030/foowiki/update",
+    serverRootPath: "/foowiki/",
+    // sparqlQueryEndpoint: "https://hyperdata.it:3030/foowiki/sparql?query=", // move ? part ??
+    // sparqlUpdateEndpoint: "http://fuseki.local:3030/foowiki/update",
 
-       runnableFormats: [
-       "http://purl.org/NET/mediatypes/application/javascript"
-   ],
+    sparqlQueryEndpoint: "https://hyperdata.it:3030/foowiki/sparql?query=", // move ? part ??
+    sparqlUpdateEndpoint: "https://hyperdata.it:3030/foowiki/update",
 
-       preformatFormats: [
-       "http://purl.org/NET/mediatypes/application/javascript",
-           "http://purl.org/NET/mediatypes/text/html"
-   ]
+    runnableFormats: [
+        "http://purl.org/NET/mediatypes/application/javascript"
+    ],
 
-   }
+    preformatFormats: [
+        "http://purl.org/NET/mediatypes/application/javascript",
+        "http://purl.org/NET/mediatypes/text/html"
+    ]
+
+}
